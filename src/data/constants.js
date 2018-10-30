dc1737971751fed09f5dff90275a5f4c46473ccd
drobnik <drobniklukasz@gmail.com>
2018-10-30 15:16:14 +0100

    add constants.js

---
+++ src/data/constants.js
@@ -0,0 +1,26 @@
+//specifies which sections are visible [empty sections are hidden]
+
+export const sections = {
+    restauracje: false,
+    przemyslowe: true,
+    domy: false,
+    renowacje: true
+};
+
+//specifies the content of all section elements
+
+
+export const portfolio = {
+    restauracje: {
+
+    },
+    przemyslowe: {
+
+    },
+    domy: {
+
+    },
+    renowacje: {
+
+    }
+};
