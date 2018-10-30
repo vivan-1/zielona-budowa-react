25c17319d0827872314b325a5287268f7c2b1d80
drobnik <drobniklukasz@gmail.com>
2018-10-30 15:40:14 +0100

    create the portfolio constant

--- src/data/constants.js
+++ src/data/constants.js
@@ -3,24 +3,44 @@
 export const sections = {
     restauracje: false,
     przemyslowe: true,
-    domy: false,
+    domy: true,
     renowacje: true
 };
 
+
 //specifies the content of all section elements
 
-
 export const portfolio = {
-    restauracje: {
-
-    },
-    przemyslowe: {
-
-    },
-    domy: {
-
-    },
-    renowacje: {
-
-    }
+    restauracje: [
+    ],
+    przemyslowe: [
+        {
+            id: 'malarnia',
+            name: 'malarnia proszkowa',
+            numberOfViews: 10,
+            videoFrame: 5,
+            videoUrl: "https://www.youtube.com/watch?v=wxPkxKkHDgQ&t=3s",
+            textFrames: [9, 10]
+        }
+    ],
+    domy: [
+        {
+            id: 'dom-poreba',
+            name: 'dom w porębie',
+            numberOfViews: 16,
+            videoFrame: 1000,
+            videoUrl: "",
+            textFrames: []
+        }
+    ],
+    renowacje: [
+        {
+            id: 'sportowa20',
+            name: 'sportowa 20',
+            numberOfViews: 17,
+            videoFrame: 1000,
+            videoUrl: "",
+            textFrames: []
+        }
+    ]
 };
