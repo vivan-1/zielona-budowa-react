7429287acc6e063c416eee5f53119671cf8bcc29
drobnik <drobniklukasz@gmail.com>
2018-10-31 21:36:22 +0100

    add routing to main panel

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,8 @@ class App extends Component {
             <Router>
                 <div id="app">
                     <Route exact path="/" component={Home}/>
-                    <Route path="/portfolio/:section/:project/:view" component={Home}/>
+                    <Route exact path="/portfolio/:section/:project/:view" component={Home}/>
+
                 </div>
             </Router>
         );
