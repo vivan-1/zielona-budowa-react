f10311a23a588ada6d6bf6e65d60487dc7ef3956
drobnik <drobniklukasz@gmail.com>
2018-11-01 11:06:23 +0100

    create retrieveProjectData method in Viewer component

--- src/components/Viewer.jsx
+++ src/components/Viewer.jsx
@@ -24,6 +24,8 @@ class Viewer extends Component {
         };
 
         this.goToUrl = this.goToUrl.bind(this);
+        this.setDetails = this.setDetails.bind(this);
+        this.retrieveProjectData = this.retrieveProjectData.bind(this);
     }
 
 
@@ -34,27 +36,15 @@ class Viewer extends Component {
 
     }
 
+    retrieveProjectData(section, project) {
 
-    //take project details from url
-    setDetails() {
-
-        let {section, project, view} = this.props.match.params;
-
-        //take the section and project names as well as view number from the url
-        this.setState({section, project, view});
-
-        //create image url based on project name and view number
-        this.setState({imageUrl: project + "-" + view});
-
-        //check the index of the project in the potfolio and get appropiate data
-
-        let projectIndex = 0;
-        switch (this.state.section) {
+        switch (section) {
             case "restauracje":
                 for (let i = 0; i < portfolio.restauracje.length; i++) {
+
                     if (portfolio.restauracje[i].id === project) {
-                        projectIndex = i;
-                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.restauracje[projectIndex];
+
+                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.restauracje[i];
                         this.setState({numberOfViews, videoFrame, videoUrl, textFrames});
                         break;
                     }
@@ -64,8 +54,8 @@ class Viewer extends Component {
             case "przemyslowe":
                 for (let i = 0; i < portfolio.przemyslowe.length; i++) {
                     if (portfolio.przemyslowe[i].id === project) {
-                        projectIndex = i;
-                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.przemyslowe[projectIndex];
+                        console.log("section ", section, " project ", project, " id: ", portfolio.przemyslowe[i].id);
+                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.przemyslowe[i];
                         this.setState({numberOfViews, videoFrame, videoUrl, textFrames});
                         break;
                     }
@@ -75,8 +65,8 @@ class Viewer extends Component {
             case "domy":
                 for (let i = 0; i < portfolio.domy.length; i++) {
                     if (portfolio.domy[i].id === project) {
-                        projectIndex = i;
-                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.domy[projectIndex];
+
+                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.domy[i];
                         this.setState({numberOfViews, videoFrame, videoUrl, textFrames});
                         break;
                     }
@@ -86,8 +76,8 @@ class Viewer extends Component {
             case "renowacje":
                 for (let i = 0; i < portfolio.renowacje.length; i++) {
                     if (portfolio.renowacje[i].id === project) {
-                        projectIndex = i;
-                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.renowacje[projectIndex];
+
+                        let {numberOfViews, videoFrame, videoUrl, textFrames} = portfolio.renowacje[i];
                         this.setState({numberOfViews, videoFrame, videoUrl, textFrames});
                         break;
                     }
@@ -95,9 +85,27 @@ class Viewer extends Component {
                 break;
         }
 
+}
+
+
+    //take project details from url
+    setDetails() {
+
+        let {section, project, view} = this.props.match.params;
+
+
+        //take the section and project names as well as view number from the url
+        this.setState({section, project, view});
+
+        //create image url based on project name and view number
+        this.setState({imageUrl: project + "-" + view});
+
+        //check the index of the project in the potfolio and get appropiate data
+        this.retrieveProjectData(section, project);
+
         //check if the current frame is the first or last frame
-        let isFirstFrame = this.state.view === 1;
-        let isLastFrame = this.state.view === this.state.numberOfViews;
+        let isFirstFrame = Number(this.state.view) === 1;
+        let isLastFrame = Number(this.state.view) === this.state.numberOfViews;
 
         //check if the current frame is a video frame
         let isVideoFrame = this.state.view === this.videoFrame;
@@ -131,9 +139,12 @@ class Viewer extends Component {
     }
 
     render() {
-        let display = this.state.section + " " + this.state.project + " " + this.state.view;
+        // let display = this.state.section + " " + this.state.project + " " + this.state.view;
 
+        // let display = "isFirstFrame: " +  (Number(this.state.view) === 1) + " view:" + Number(this.state.view) + " isLastFrame: " + this.state.isLastFrame + " numberOfViews: " + this.state.numberOfViews;
         //hide back and next buttons when they are not necessary
+
+        let display = "numberOfViews: " + this.state.numberOfViews + " videoFrame: " + this.state.videoFrame + " videoUrl: " + this.state.videoUrl + " textFrames: " + this.state.textFrames;
         let backButton = this.state.isFirst ? "<div></div>" : <a id="prev-link" href="#"><span
                             aria-hidden="true">&larr;</span> Wstecz</a>;
 
