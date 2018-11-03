8d9b8f59ef19c2aa2cca19d1dbd3fcd13dfcf69e
drobnik <drobniklukasz@gmail.com>
2018-11-03 15:56:04 +0100

    change email address

--- src/components/MenuPanel.jsx
+++ src/components/MenuPanel.jsx
@@ -213,7 +213,7 @@ class MenuPanel extends Component {
                     </div>
                     <div id="footer">
                         <p>zielona budowa<br/><span className="small-text">+48 664 142 159<br/><a
-                            href="mailto:zielonabudowa@gmail.com">zielonabudowa@gmail.com</a><br/></span><a
+                            href="mailto:biuro@zielonabudowa.com.pl">biuro@zielonabudowa.com.pl</a><br/></span><a
                             href="https://web.facebook.com/zielonabudowa/" target="_blank"><i
                             className="fa fa-facebook-square" aria-hidden="true"></i></a>&nbsp;&nbsp;<a
                             href="https://www.instagram.com/zielonabudowa/" target="_blank"><i
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -227,7 +227,7 @@ class NavigationBar extends Component {
 
                             <div id="nav-footer">
                                 <p>zielona budowa<br/><span className="small-text">+48 664 142 159<br/><a
-                                    href="mailto:zielonabudowa@gmail.com">zielonabudowa@gmail.com</a><br/></span><a
+                                    href="mailto:biuro@zielonabudowa.com.pl">biuro@zielonabudowa.com.pl</a><br/></span><a
                                     href="https://web.facebook.com/zielonabudowa/" target="_blank"><i
                                     className="fa fa-facebook-square"
                                     aria-hidden="true"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a
