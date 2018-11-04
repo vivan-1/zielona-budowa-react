6841060c3d98a8566825e1ba3bbc1a0967905926
drobnik <drobniklukasz@gmail.com>
2018-11-04 22:34:50 +0100

    update readme

--- src/components/iframe.jsx
+++
@@ -1,7 +0,0 @@
-content = <div className="view-content">
-    <div className="video-wrapper">
-        <div className="embed-responsive">
-            <iframe width="520" height="293" src={this.state.videoUrl} frameBorder="0" allowFullScreen></iframe>
-        </div>
-    </div>
-</div>;
