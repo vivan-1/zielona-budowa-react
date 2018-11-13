52513f3bd93b922d6a223b2e26400aced2e29c00
drobnik <drobniklukasz@gmail.com>
2018-11-13 20:37:04 +0100

    add malarnia text-note

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,49 @@ export const portfolio = {
             name: 'malarnia proszkowa',
             numberOfViews: 9,
             textFrame: 9,
-            textContent: <div><h1>Commodi doloribus expedita natus quam soluta</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem commodi corporis cupiditate ex fugiat harum incidunt minima nam nisi omnis praesentium saepe, voluptatem, voluptatum. Aut, dicta excepturi. Ab dolor, itaque!</p><p className="centered">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, magnam, reiciendis!</p></div>,
+            textContent:
+                <div>
+                    <h1>Zestawienie powierzchni zagospodarowania działki</h1>
+                    <table>
+                        <tr className="underlined">
+                            <td>Całkowita powierzchnia działki:</td>
+                            <td>6088,19 m<sup>2</sup></td>
+                            <td>(100%)</td>
+                        </tr>
+                        <tr>
+                            <td>Powierzchnia zabudowy kubaturowej projektowanej:</td>
+                            <td>2847,52 m<sup>2</sup></td>
+                            <td>(46,77%)</td>
+                        </tr>
+                        <tr>
+                            <td>Powierzchnia utwardzonych nawierzchni:</td>
+                            <td>2022,17 m<sup>2</sup></td>
+                            <td>(33,22%)</td>
+                        </tr>
+                        <tr>
+                            <td>Powierzchnia biologicznie czynna:</td>
+                            <td>1218,50 m<sup>2</sup></td>
+                            <td>(20,01%)</td>
+                        </tr>
+
+                    </table>
+
+                    <p className="wide-margins">
+                        w tym:<br/>
+                        - powierzchnia biologicznie czynna z trawników: 994,25 m<sup>2</sup><br/>
+                        - w tym 50% obszaru 39 miejsc parkingowych o powierzchni ażurowej: 448,5 m<sup>2</sup> ˟ 50% = 224,25 m<sup>2</sup>
+                    </p>
+
+
+                    <p className="centered">Spełnia się zatem zasady kształtowania zabudowy oraz wskaźniki zagospodarowania terenu:</p>
+                    <p className="wide-margins">
+                        1) wskaźnik powierzchni zabudowy – do 80% powierzchni działki<br/>
+                        2) intensywność zabudowy – od 0,2 do 2,0<br/>
+                        3) minimalny udział procentowy powierzchni biologicznie czynnej - 20% powierzchni działki zagospodarowanej w formie zieleni urządzonej
+                    </p>
+
+                    <p className="small-text centered">UCHWAŁY NR 353/XXXIV/08 R. RADY MIEJSKIEJ W LUBLIŃCU z dnia 26 listopada 2008 r. w sprawie: miejscowego planu zagospodarowania przestrzennego dla terenu we wschodniej części Lublińca. (MPZP)</p>
+                </div>,
             fileFrame: 0,
             fileContent: ""
         },
