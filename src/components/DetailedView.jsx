220280d5b7659929961237e233790c26584b293b
drobnik <drobniklukasz@gmail.com>
2018-11-25 17:55:34 +0100

    work on modal in DetailedView comp

--- src/components/DetailedView.jsx
+++ src/components/DetailedView.jsx
@@ -1,8 +1,17 @@
 import React, {Component} from "react";
+import {Modal} from "react-bootstrap";
+import {ScaleLoader} from "react-spinners"; //spinner
 import {concepts} from "../data/constants";
 
 class DetailedView extends Component {
+constructor(props) {
+    super(props);
 
+    this.state = {
+        show: true, //specifies whether the detailed view modal should be visible
+        // loading: true, //specifies whether the spinner should be visible
+    }
+}
 
 
     render() {
@@ -11,6 +20,7 @@ class DetailedView extends Component {
         let videoName = ""; //video name
         let videoUrl = ""; //video url based on video id
 
+        //look through the concepts array to check the video name and url
         for (let i = 0; i < concepts.length; i++) {
 
             if (concepts[i].id === this.props.match.params.video) {
@@ -20,9 +30,38 @@ class DetailedView extends Component {
             }
         }
 
+
+        // let containerClass = ""; //specifies the class of the container holding the main content
+        // let loaderClass = ""; //specifies the class of the container holding the spinner
+
+        // if (this.state.loading) {
+        //     containerClass = "displayNone"; //hides the modal content when loading
+        //     loaderClass = "loaderViewContainer"; //shows the loader when loading
+        // } else {
+        //     containerClass = "contentContainer"; //shows the modal content when loaded
+        //     loaderClass = "displayNone"; //hides the loader when the content has loaded
+        // }
+
         return(
             <div>
-                {videoId}
+                <Modal
+                    show={this.state.show}
+                    onHide={this.handleClose}
+                    dialogClassName="conceptView"
+
+                >
+
+                        <Modal.Header closeButton>
+                            <Modal.Title><strong>{videoName}</strong></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body className="detailBody">
+                            <div className="detailContentContainer">
+                                {videoUrl}
+
+                            </div>
+                        </Modal.Body>
+
+                </Modal>
         </div>
         );
     }
